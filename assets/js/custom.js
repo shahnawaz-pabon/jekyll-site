f7c11c58d627b9c72f94ad72f053ca6ae20454c0
shahnawaz-pabon <s.pabon93@gmail.com>
2023-09-08 20:37:17 +0600

    script added for the current year

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,88 +1,86 @@
-(function($) {
+(function ($) {
+  document.getElementById("currentYear").textContent = new Date().getFullYear();
 
-  if(localStorage.theme=='dark'){
+  if (localStorage.theme == "dark") {
     $("body").addClass("dark");
-    $('.light-mode').css('display','');
-    $('.dark-mode').css('display','none');
-  }
-  else{
+    $(".light-mode").css("display", "");
+    $(".dark-mode").css("display", "none");
+  } else {
     $("body").removeClass("dark");
-    $('.light-mode').css('display','none');
-    $('.dark-mode').css('display','');
+    $(".light-mode").css("display", "none");
+    $(".dark-mode").css("display", "");
   }
 
   // $("#toggle-dark").click();
 
-  $(function() {
+  $(function () {
     var header = $(".start-style");
-    $(window).scroll(function() {
+    $(window).scroll(function () {
       var scroll = $(window).scrollTop();
 
       if (scroll >= 10) {
-        header.removeClass('start-style').addClass("scroll-on");
+        header.removeClass("start-style").addClass("scroll-on");
       } else {
-        header.removeClass("scroll-on").addClass('start-style');
+        header.removeClass("scroll-on").addClass("start-style");
       }
     });
   });
 
   //Animation
 
-  $(document).ready(function() {
-    $('body.hero-anime').removeClass('hero-anime');
+  $(document).ready(function () {
+    $("body.hero-anime").removeClass("hero-anime");
   });
 
   //Menu On Hover
 
-  $('body').on('mouseenter mouseleave','.nav-item',function(e){
-      if ($(window).width() > 750) {
-        var _d=$(e.target).closest('.nav-item');_d.addClass('show');
-        setTimeout(function(){
-        _d[_d.is(':hover')?'addClass':'removeClass']('show');
-        },1);
-      }
+  $("body").on("mouseenter mouseleave", ".nav-item", function (e) {
+    if ($(window).width() > 750) {
+      var _d = $(e.target).closest(".nav-item");
+      _d.addClass("show");
+      setTimeout(function () {
+        _d[_d.is(":hover") ? "addClass" : "removeClass"]("show");
+      }, 1);
+    }
   });
 
   //Switch light/dark
 
-  $("#toggle-dark").on('click', function () {
-    if (localStorage.theme=='dark') {
+  $("#toggle-dark").on("click", function () {
+    if (localStorage.theme == "dark") {
       $("body").removeClass("dark");
       // $("#toggle-dark").removeClass("switched");
-      $('.light-mode').css('display','none');
-      $('.dark-mode').css('display','');
+      $(".light-mode").css("display", "none");
+      $(".dark-mode").css("display", "");
 
-      localStorage.theme = 'light';
-    }
-    else {
+      localStorage.theme = "light";
+    } else {
       $("body").addClass("dark");
 
-      $('.light-mode').css('display','');
-      $('.dark-mode').css('display','none');
+      $(".light-mode").css("display", "");
+      $(".dark-mode").css("display", "none");
 
-      localStorage.theme = 'dark';
-
+      localStorage.theme = "dark";
     }
   });
 
   /* Nav items active on click */
-  $(".navbar-nav li").click(function(event) {
+  $(".navbar-nav li").click(function (event) {
     var $this = $(this);
 
     console.log($(this));
     $(".navbar-nav li").removeClass("active");
-    $this.removeClass('active');
+    $this.removeClass("active");
     $($this).addClass("active");
   });
-    //Click event handler for nav-items
-    // $('.nav-item').on('click',function(){
-    //
-    //   //Remove any previous active classes
-    //   $('.nav-item').removeClass('active');
-    //
-    //   //Add active class to the clicked item
-    //   $(this).addClass('active');
-    // });
+  //Click event handler for nav-items
+  // $('.nav-item').on('click',function(){
+  //
+  //   //Remove any previous active classes
+  //   $('.nav-item').removeClass('active');
+  //
+  //   //Add active class to the clicked item
+  //   $(this).addClass('active');
+  // });
   /* Nav items active on click */
-
-  })(jQuery);
+})(jQuery);
